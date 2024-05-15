8da27e4d3895a7f04a4b4ba8afa6a5619c0aebfc
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-05-15 22:27:43 +0200

    set up clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Navbar from "@/ui/molecules/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { Inter } from "next/font/google";
 
@@ -20,13 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={`font-sans ${inter.variable} text-primary-700 px-6 pt-7 max-w-[1077px] mx-auto`}
-      >
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`font-sans ${inter.variable} text-primary-700 px-6 pt-7 max-w-[1077px] mx-auto`}
+        >
+          <Navbar />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
