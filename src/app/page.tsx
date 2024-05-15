94ff7fe7952b18ca45fea6c4d1d41e9eb5d67d6d
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-05-15 22:02:00 +0200

    changed title and description of the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Content from "./_components/Content";
 import Pricing from "./_components/Pricing";
 import CTASection from "./_components/CTASection";
 import Footer from "@/ui/molecules/Footer";
-import PromptGenerator from "@/ui/organisms/PromptGenerator";
 
 export default async function HomePage() {
+  const url =
+    "https://cdn.creatomate.com/renders/0f4aade0-521d-4059-8f08-751a83c27f79.mp4";
+  console.log(url.length);
   return (
     <main className="relative min-h-screen h-screen z-0">
       <Hero />
