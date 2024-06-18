00f8cf0e7dcef4c74495f4e8774222686be8df10
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-06-18 15:32:44 +0200

    saving messages in db

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,10 @@
 import { openai } from "@ai-sdk/openai";
 import { StreamingTextResponse, streamText, type CoreMessage } from "ai";
+import { v4 as uuidv4 } from "uuid";
 
 import { type NextRequest } from "next/server";
+import { db } from "@/server/db";
+import { messages as messagesDb } from "@/server/db/schema";
 
 export async function POST(req: NextRequest): Promise<StreamingTextResponse> {
   const { messages, conversation_id } = (await req.json()) as {
@@ -19,7 +22,12 @@ export async function POST(req: NextRequest): Promise<StreamingTextResponse> {
   const stream = result.toAIStream({
     async onFinal(data) {
       // Save the message to the database
-      console.log(data);
+      await db.insert(messagesDb).values({
+        id: uuidv4() as string, // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+        conversationId: conversation_id as string,
+        role: "assistant",
+        content: data,
+      });
     },
   });
 
