285f1d6dd17208709dae9f295b133b8868405b0a
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-06-19 17:33:40 +0200

    set up the chatbot component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import Footer from "@/ui/molecules/Footer";
-import PromptGenerator from "@/ui/organisms/PromptGenerator";
-import CTASection from "./_components/CTASection";
-import Content from "./_components/Content";
-import Hero from "./_components/Hero";
-import Pricing from "./_components/Pricing";
-import { currentUser } from "@clerk/nextjs/server";
 import { db } from "@/server/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { conversations } from "@/server/db/schema";
+
+// Components of the page
+import Hero from "./_components/Hero";
+import Content from "./_components/Content";
+import Pricing from "./_components/Pricing";
+import CTASection from "./_components/CTASection";
+import Footer from "@/ui/molecules/Footer";
+
+// Ui components
+import Chatbot from "@/ui/organisms/Chatbot";
 
 export default async function HomePage() {
   const user = await currentUser();
@@ -27,12 +31,12 @@ export default async function HomePage() {
 
   return (
     <main className="relative min-h-screen h-screen z-0">
-      <PromptGenerator conversation={conversation} />
       <Hero />
       <Content />
       <Pricing />
       <CTASection />
       <Footer />
+      <Chatbot />
     </main>
   );
 }
