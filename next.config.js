1d1d654d67fce9c2770191184c59d2466070156a
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-07-03 22:11:53 +0200

    switched img for Images in content

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  images: {
+    domains: ["images.unsplash.com"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
