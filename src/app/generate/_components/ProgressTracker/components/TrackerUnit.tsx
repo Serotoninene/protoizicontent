8ada8b351e36ff93e3c3ab55d3df134fd3dcf3c4
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-07-05 10:42:20 +0200

    fixed type error for Tracker State + set up the basic transition between the states in TrackerUnit

--- src/app/generate/_components/ProgressTracker/components/TrackerUnit.tsx
+++ src/app/generate/_components/ProgressTracker/components/TrackerUnit.tsx
@@ -1,15 +1,25 @@
-import { useEffect } from "react";
+import clsx from "clsx";
+import type { TrackerStates } from "../index";
 
 type Props = {
   label: string;
   index: number;
-  state: "default" | "active" | "completed";
+  state?: TrackerStates;
 };
 
 const TrackerUnit = ({ label, index, state }: Props) => {
   return (
     <div className="flex gap-2 items-center">
-      <div className="w-6 h-6 flex justify-center items-center border border-primary-700 rounded-full text-xs text-primary-700">
+      <div
+        className={clsx(
+          "w-6 h-6 flex justify-center items-center rounded-full text-xs text-primary-700 transition duration-300 ease-in-out",
+          {
+            "text-primary-700 border border-primary-700": state === "default",
+            "bg-primary-700 text-white ": state === "active",
+            "bg-secondary-400 text-white": state === "completed",
+          },
+        )}
+      >
         {index}
       </div>
       <p className="text-sm font-semibold">{label}</p>
--- src/app/generate/_components/ProgressTracker/index.tsx
+++ src/app/generate/_components/ProgressTracker/index.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import { useState } from "react";
 import TrackerUnit from "./components/TrackerUnit";
 
+export type TrackerStates = "default" | "active" | "completed";
+
 export default function ProgressTracker() {
+  const states: TrackerStates[] = ["default", "active", "completed"];
+  const [currentState, setCurrentState] = useState<TrackerStates | undefined>(
+    states[0],
+  );
+
+  const moveStepForward = () => {
+    if (!currentState) return;
+
+    const currentIndex = states.indexOf(currentState);
+    if (currentIndex < states.length - 1) {
+      setCurrentState(states[currentIndex + 1]);
+    }
+  };
+
   return (
-    <div className="absolute blurred_background-2xl flex px-8 py-4 bottom-6 left-[50%] -translate-x-1/2 rounded-full">
-      <TrackerUnit index={1} label="Start" state="active" />
+    <div
+      onClick={moveStepForward}
+      className="absolute blurred_background-2xl flex px-8 py-4 bottom-6 left-[50%] -translate-x-1/2 rounded-full"
+    >
+      <TrackerUnit index={1} label="Start" state={currentState} />
     </div>
   );
 }
