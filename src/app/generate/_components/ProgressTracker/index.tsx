7f138a62b6a14ed1e15d6bccedc1c20c30a203c6
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-07-05 15:36:48 +0200

    added blurred gradient to progress tracker

--- src/app/generate/_components/ProgressTracker/index.tsx
+++ src/app/generate/_components/ProgressTracker/index.tsx
@@ -10,32 +10,36 @@ export default function ProgressTracker() {
   const [currentStep, setCurrentStep] = useState(0);
 
   const moveStepForward = () => {
-    setCurrentStep((prevStep) => Math.min(prevStep + 1, steps.length - 1));
+    setCurrentStep((prevStep) => Math.min(prevStep + 1, steps.length));
   };
 
   return (
-    <div
-      onClick={moveStepForward}
-      className="absolute blurred_background-2xl flex gap-2 px-8 py-4 bottom-6 left-[50%] -translate-x-1/2 rounded-full"
-    >
-      {steps.map((label, index) => {
-        let state: TrackerState = "default";
-        if (index < currentStep) {
-          state = "completed";
-        } else if (index === currentStep) {
-          state = "active";
-        }
+    <div className="absolute bottom-6 left-[50%] -translate-x-1/2">
+      <div className="absolute h-1/3 w-1/4 top-2 left-2 bg-secondary-400 rounded-full" />
+      <div className="absolute h-1/3 w-1/4 bottom-2 right-2 bg-secondary-400 rounded-full" />
+      <div
+        onClick={moveStepForward}
+        className="blurred_background-xl flex gap-2 px-8 py-4 rounded-full"
+      >
+        {steps.map((label, index) => {
+          let state: TrackerState = "default";
+          if (index < currentStep) {
+            state = "completed";
+          } else if (index === currentStep) {
+            state = "active";
+          }
 
-        return (
-          <TrackerUnit
-            key={label}
-            index={index + 1}
-            label={label}
-            state={state}
-            isLast={index === steps.length - 1}
-          />
-        );
-      })}
+          return (
+            <TrackerUnit
+              key={label}
+              index={index + 1}
+              label={label}
+              state={state}
+              isLast={index === steps.length - 1}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
