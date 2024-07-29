9f14a8c39e200f1c561325d73ac66162c3569996
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-07-29 15:47:44 +0200

    removed the generate function from ai context

--- src/server/actions/ai.tsx
+++ src/server/actions/ai.tsx
@@ -106,40 +106,10 @@ export async function continueConversation(
   };
 }
 
-export async function generate() {
-  const stream = createStreamableValue();
-
-  (async () => {
-    const { partialObjectStream } = await streamObject({
-      model: openai("gpt-4-turbo"),
-      system: "You generate three notifications for a messages app.",
-      prompt: "Generate three notifications for a messages app.",
-      schema: z.object({
-        notifications: z.array(
-          z.object({
-            name: z.string().describe("Name of a fictional person."),
-            message: z.string().describe("Do not use emojis or links."),
-            minutesAgo: z.number(),
-          }),
-        ),
-      }),
-    });
-
-    for await (const partialObject of partialObjectStream) {
-      stream.update(partialObject);
-    }
-
-    stream.done();
-  })();
-
-  return { object: stream.value };
-}
-
 export const AI = createAI<ServerMessage[], ClientMessage[]>({
   actions: {
     continueConversation,
     generateContent,
-    generate,
   },
   onSetAIState: async ({ state, done }) => {
     const user = await currentUser();
