935e1b62e3205097809fa7eb8d161ac72dde984c
alexandre.pujol <pujol.alexandre@hotmail.fr>
2024-07-29 16:22:57 +0200

    repositionned the sidebar to be always against the left side of the screené

--- src/app/generate/_components/GenerateForm/index.tsx
+++ src/app/generate/_components/GenerateForm/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { generate } from "@/server/actions/ai";
 
 /* eslint-disable */
 
--- src/app/generate/layout.tsx
+++ src/app/generate/layout.tsx
@@ -3,7 +3,9 @@ import Sidebar from "@/ui/molecules/Sidebar";
 export default function Layoot({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-[calc(100vh-56px)] md:gap-6">
-      <Sidebar />
+      <div className="absolute left-2 top-2 bottom-2">
+        <Sidebar />
+      </div>
       <div className="flex-grow">{children}</div>
     </div>
   );
